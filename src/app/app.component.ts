328f9a459a094c48d985b2e68b38b75101fe51e4
Heitor Miranda <heitormiranda@Heitors-MacBook-Pro.local>
2024-04-09 11:02:11 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'ts-amplify';
+  title = "ts-amplify";
+
+  constructor() {
+    console.log({ subdomain: this.getSubdomain() });
+  }
+
+  getSubdomain = () => {
+    const domain = window.location.hostname;
+    let subdomain = "";
+    if (
+      domain.indexOf(".") < 0 ||
+      domain.split(".")[0] === "portal" ||
+      domain.split(".")[0] === "dev" ||
+      domain.split(".")[0] === "dashboard"
+    ) {
+      subdomain = "";
+    } else {
+      subdomain = domain.split(".")[0];
+    }
+    return subdomain;
+  };
 }
